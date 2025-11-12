98dfcb64d9290429ee646558610cfd571f585841
studentharsh2007-source <studentharsh2007@gmail.com>
2025-11-12 11:43:17 +0530

    Add files via upload
    uppercase or lower case

---
+++ variabl.cpp
@@ -0,0 +1,14 @@
+//find tha lowercase and upper case
+#include<iostream>
+using namespace std;
+int main(){
+    char ch='h';
+    if(ch>='a'&&'z'>=ch){
+        cout<<"lower case"<<" ";
+    }else{
+        cout<<"upper case";
+    }
+    int n=65;
+    cout<<(n>=0?"harsh":"gupta");
+    return 0;
+}
