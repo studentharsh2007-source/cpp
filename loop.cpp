27c1a39ac213dfcabfd520e733697413d53d8891
studentharsh2007-source <studentharsh2007@gmail.com>
2025-11-13 13:14:16 +0530

    Add files via upload
    loop in cpp

---
+++ loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    int count=0;
+    do{
+        cout<<i;
+        i++;
+        
+    }while(i<n);
+}
